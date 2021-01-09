396337417c9c7ce3fe26a4d4c282106b5aeb7a75
protagonisths <13837050616@163.com>
2021-01-09 23:30:16 +0800

    Update usage.lua

--- setup/usage.lua
+++ setup/usage.lua
@@ -2,3 +2,19 @@ local myMod = require("setup.modules")
 
 print(myMod.add(1, 2))
 print(myMod.hi("world"))
+
+local temp = {10, 12}
+for index, value in ipairs(temp) do
+    print(value)
+end
+
+local function myAddition(...)
+    local sum = 0
+    for index, value in ipairs{...} do
+        sum = sum + value
+    end
+    return sum
+end
+
+print(myAddition(1, 2, 3, 4, 5))
+print(myAddition(1, 2))
