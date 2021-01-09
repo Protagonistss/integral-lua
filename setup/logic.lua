415ac071e448544fbf2d4b2b4c9973b2fc354707
protagonisths <13837050616@163.com>
2021-01-09 14:00:00 +0800

    Create logic.lua
    logic

---
+++ setup/logic.lua
@@ -0,0 +1,24 @@
+local condition = 0
+
+if condition == 1 then
+    print("Equal if-then 1")
+end
+if condition <= 1 then
+    print("Equal if-then less 1")
+end
+
+if condition >=1 then
+    print("Equal if-then than 1")
+end
+
+if condition ~= 1 then
+    print("Not Equal if-then")
+end
+
+if condition == 1 then
+    print("1 true") 
+elseif condition == 0 then
+    print("0 true")
+else
+    print("not match")
+end
